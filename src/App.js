31d8d2548ac23a5265c343905065999881a343b3
Matt Voggel <64497380+mvoggel@users.noreply.github.com>
2025-02-09 08:20:01 -0500

    updates to setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Sidebar from "./components/Sidebar";
-import Home from "./pages/Home";
-import Probability from "./pages/Probability";
-import Statistics from "./pages/Statistics";
-import Bayesian from "./pages/Bayesian";
+import Sidebar from "./components/sidebar.js";
+import Navbar from "./components/navbar.js";
+import Home from "./pages/home.js";
+import Probability from "./pages/probability.js";
+import Statistics from "./pages/statistics.js";
+import Bayesian from "./pages/bayesian.js";
+import Footer from "./components/footer.js";
 
 function App() {
   return (
